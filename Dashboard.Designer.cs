ba93714657649cb1d12bff2b345ba956a454a158
Charlls Ruiz <ruizlorrenz@gmail.com>
2022-04-06 21:17:56 -0600

    program functioning but not completely done, check TODO.

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -30,6 +30,7 @@
             this.weightTextbox = new System.Windows.Forms.TextBox();
             this.resultLabel = new System.Windows.Forms.Label();
             this.calculateButton = new System.Windows.Forms.Button();
+            this.classificationLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // programNameTextbox
@@ -46,9 +47,9 @@
             this.heightLabel.AutoSize = true;
             this.heightLabel.Location = new System.Drawing.Point(15, 51);
             this.heightLabel.Name = "heightLabel";
-            this.heightLabel.Size = new System.Drawing.Size(108, 24);
+            this.heightLabel.Size = new System.Drawing.Size(97, 24);
             this.heightLabel.TabIndex = 1;
-            this.heightLabel.Text = "Height (cm)";
+            this.heightLabel.Text = "Height (in)";
             // 
             // weightLabel
             // 
@@ -78,9 +79,8 @@
             this.resultLabel.AutoSize = true;
             this.resultLabel.Location = new System.Drawing.Point(15, 205);
             this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(72, 24);
+            this.resultLabel.Size = new System.Drawing.Size(0, 24);
             this.resultLabel.TabIndex = 5;
-            this.resultLabel.Text = "Result: ";
             // 
             // calculateButton
             // 
@@ -92,11 +92,22 @@
             this.calculateButton.UseVisualStyleBackColor = true;
             this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
             // 
+            // classificationLabel
+            // 
+            this.classificationLabel.AutoSize = true;
+            this.classificationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.classificationLabel.Location = new System.Drawing.Point(19, 258);
+            this.classificationLabel.Name = "classificationLabel";
+            this.classificationLabel.Size = new System.Drawing.Size(86, 16);
+            this.classificationLabel.TabIndex = 7;
+            this.classificationLabel.Text = "Classification";
+            // 
             // bmiCalculatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(233, 250);
+            this.ClientSize = new System.Drawing.Size(266, 286);
+            this.Controls.Add(this.classificationLabel);
             this.Controls.Add(this.calculateButton);
             this.Controls.Add(this.resultLabel);
             this.Controls.Add(this.weightTextbox);
@@ -105,7 +116,7 @@
             this.Controls.Add(this.heightLabel);
             this.Controls.Add(this.programNameTextbox);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "bmiCalculatorForm";
             this.Text = "BMI Calculator";
             this.ResumeLayout(false);
@@ -122,6 +133,7 @@
         private System.Windows.Forms.TextBox weightTextbox;
         private System.Windows.Forms.Label resultLabel;
         private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label classificationLabel;
     }
 }
 
--- Dashboard.cs
+++ Dashboard.cs
@@ -7,12 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-namespace SimpleBMICalculator {
+/// <summary>
+/// @Author: Charlls Ruiz
+/// Last Modified: April 6 2022
+/// 
+/// Description: A simple BMI calculator that uses height in pounds and weight in inches to calculate the BMI.
+/// TODO: Implement the ability to use feet and inches as height and kilograms as weight.
+/// </summary>
+namespace SimpleBMICalculator { 
     public partial class bmiCalculatorForm : Form {
         private int height,
-                    weight,
-                    bmi;
+                    weight;
+        private double bmi;
         public bmiCalculatorForm() {
             InitializeComponent();
         }
@@ -21,9 +27,31 @@ namespace SimpleBMICalculator {
             height = int.Parse(heightTextbox.Text);
             weight = int.Parse(weightTextbox.Text);
 
-            bmi = height + weight;
+            bmi = CalculateBMI(height, weight);
 
-            resultLabel.Text = $"test result: {bmi}";
+            resultLabel.Text = $"BMI: {bmi}";
+            classificationLabel.Text = "You are considered: " + GetClassification(bmi);
+        }
+
+
+        private double CalculateBMI(int height, int weight) {
+            double result = 1.0 * (703 * weight) / (height * height);
+
+            return Math.Round(result, 1);
+        }
+
+        private string GetClassification(double bmi) {
+            if (bmi < 18.5) {
+                return "Underweight";
+            } else if (bmi >= 18.5 && bmi <= 24.9) {
+                return "Healthy";
+            } else if (bmi >= 25 && bmi <= 29.9) {
+                return "Overweight";
+            } else if (bmi >= 30 && bmi <= 39.9) {
+                return "Obese";
+            } else { // bmi > 40
+                return "Extremely Obese";
+            }
         }
     }
 }
