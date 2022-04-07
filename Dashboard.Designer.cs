d523071afd10a290c6d4e11576e527970f02ace1
Charlls Ruiz <ruizlorrenz@gmail.com>
2022-04-06 20:33:39 -0600

    form designed with some test functionality

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SimpleBMICalculator {
+    partial class bmiCalculatorForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.programNameTextbox = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.heightTextbox = new System.Windows.Forms.TextBox();
+            this.weightTextbox = new System.Windows.Forms.TextBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // programNameTextbox
+            // 
+            this.programNameTextbox.AutoSize = true;
+            this.programNameTextbox.Location = new System.Drawing.Point(12, 9);
+            this.programNameTextbox.Name = "programNameTextbox";
+            this.programNameTextbox.Size = new System.Drawing.Size(193, 24);
+            this.programNameTextbox.TabIndex = 0;
+            this.programNameTextbox.Text = "Simple BMI Calculator";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Location = new System.Drawing.Point(15, 51);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(108, 24);
+            this.heightLabel.TabIndex = 1;
+            this.heightLabel.Text = "Height (cm)";
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Location = new System.Drawing.Point(15, 94);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(110, 24);
+            this.weightLabel.TabIndex = 2;
+            this.weightLabel.Text = "Weight (lbs)";
+            // 
+            // heightTextbox
+            // 
+            this.heightTextbox.Location = new System.Drawing.Point(157, 51);
+            this.heightTextbox.Name = "heightTextbox";
+            this.heightTextbox.Size = new System.Drawing.Size(47, 29);
+            this.heightTextbox.TabIndex = 3;
+            // 
+            // weightTextbox
+            // 
+            this.weightTextbox.Location = new System.Drawing.Point(158, 94);
+            this.weightTextbox.Name = "weightTextbox";
+            this.weightTextbox.Size = new System.Drawing.Size(47, 29);
+            this.weightTextbox.TabIndex = 4;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(15, 205);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(72, 24);
+            this.resultLabel.TabIndex = 5;
+            this.resultLabel.Text = "Result: ";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(74, 140);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(110, 40);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // bmiCalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 250);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.weightTextbox);
+            this.Controls.Add(this.heightTextbox);
+            this.Controls.Add(this.weightLabel);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.programNameTextbox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "bmiCalculatorForm";
+            this.Text = "BMI Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label programNameTextbox;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.TextBox heightTextbox;
+        private System.Windows.Forms.TextBox weightTextbox;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ Dashboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleBMICalculator {
+    public partial class bmiCalculatorForm : Form {
+        private int height,
+                    weight,
+                    bmi;
+        public bmiCalculatorForm() {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e) {
+            height = int.Parse(heightTextbox.Text);
+            weight = int.Parse(weightTextbox.Text);
+
+            bmi = height + weight;
+
+            resultLabel.Text = $"test result: {bmi}";
+        }
+    }
+}
